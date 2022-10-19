e2f1b1088ddd00b7be28c061480fcc1b1f3e427c
Monica Gerard <mlgvla@gmail.com>
2022-10-19 14:10:56 -0400

    solution

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,44 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_song_to_count()
+        self.add_to_genres(self.genre)
+        self.add_to_artists(self.artist)
+        self.add_to_genre_count(self.genre)
+        self.add_to_artist_count(self.artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        cls.genres.append(genre)
+        cls.genres = [*set(cls.genres)]
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        cls.artists.append(artist)
+        cls.artists = [*set(cls.artists)]
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count.update({genre: 1})
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count.update({artist: 1})
